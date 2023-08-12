1f7f314bb6e074c95700062122f06081e1df4629
pyhsudars <mail2harishsudarsanan@gmail.com>
2023-08-12 17:25:42 +0530

    Update banner.tsx

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import './banner.scss'
+import MercedesLogo from '../../assets/images/mercedes_logo.svg'
 
 const Banner: React.FC = () => {
   return (
     <>
-      <div className='p-3 bg-dark text-white '>
-        <h2 className='mx-5'>BatteRE</h2>
+      <div className='p-3 branding'>
+        <span className='logo-txt'>
+          <img className='logo' src={MercedesLogo} alt='Mercedes Logo' />
+          <h1 className='text-white h3-text'>oneSCM</h1>
+        </span>
       </div>
     </>
   )
