1e8f318acae204ac4bdc461826c561ab05cffb94
pyhsudars <mail2harishsudarsanan@gmail.com>
2023-08-12 17:29:52 +0530

    Create home.tsx

---
+++ src/containers/layout/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AppBatteryGuage from '../../components/battery-guage/battery-guage'
+
+const Home: React.FC = () => {
+  return (
+    // <div className='row'>
+    <AppBatteryGuage />
+    // </div>
+  )
+}
+
+export default Home
